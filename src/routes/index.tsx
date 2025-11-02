bda78c1116d698a7ba512af0a687e951662488e7
vividsystem <notrealbutnotobot@gmail.com>
2025-11-02 10:26:06 +0100

    feat: responsive links

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,8 +6,10 @@ export default function Home() {
 		<MetaProvider>
 			<main class="text-center mx-auto text-gray-700 p-4">
 				<Title>vividsystem</Title>
-				<h1 class="max-6-xs text-9xl text-primary font-normal">Hi, I am <br /><span class="intro hover:font-bold hover:text-vivid-pink text-vivid-blue">vividsystem</span></h1>
-				<p class="text-4xl text-secondary">A student <A class="link" href="/projects">building stuff</A> in his free-time</p>
+				<div>
+					<h1 class="text-4xl lg:text-9xl text-primary font-normal">Hi, I am <br /><span class="intro hover:font-bold hover:text-vivid-pink text-vivid-blue">vividsystem</span></h1>
+					<p class="text-2xl lg:text-4xl text-secondary">A student <A class="link" href="/projects">building stuff</A> in his free-time</p>
+				</div>
 			</main>
 		</MetaProvider>
 	);
