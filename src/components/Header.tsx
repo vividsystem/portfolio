8fec74ccf371dbe260a5bd48ef98017847d2829b
vividsystem <notrealbutnotobot@gmail.com>
2025-11-02 15:58:41 +0100

    feat: small qol improvements

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,10 @@
+import { A } from "@solidjs/router";
+
 export default function Header() {
 	return (
 		<header class="static top-0 right-0 left-0">
-			<img src="/favicon.svg" alt="vividsystem logo" class="size-16" />
+			<A href="/"><img src="/favicon.svg" alt="vividsystem logo" class="size-16" /></A>
 
-		</header>
+		</header >
 	)
 }
