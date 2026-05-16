8eb9cae7f69681e9d1a3afc944e2c3d75dd7b4d6
vividsystem <notrealbutnotobot@gmail.com>
2026-05-16 18:44:53 +0200

    feat: add meta descriptions for better SEO

--- src/routes/contact.tsx
+++ src/routes/contact.tsx
@@ -1,3 +1,4 @@
+import { Meta } from "@solidjs/meta";
 import { A } from "@solidjs/router";
 import { ArrowUpRight } from "lucide-solid";
 import { GithubIcon, SlackIcon } from "~/components/Icons";
@@ -5,6 +6,10 @@ import { GithubIcon, SlackIcon } from "~/components/Icons";
 export default function Contact() {
 	return (
 		<main class="p-4 text-primary pb-20">
+			<Meta
+				name="description"
+				content="Get in touch with vividsystem"
+			/>
 			<h1 class="text-2xl lg:text-5xl text-shadow-solid-vivid-pink underline decoration-8 pb-4">Contact</h1>
 			<p class="text-xl lg:text-4xl">Below you'll find my socials. I might take ages to respond.</p>
 			<div>
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,14 @@
-import { MetaProvider, Title } from "@solidjs/meta";
+import { Meta, Title } from "@solidjs/meta";
 import { A } from "@solidjs/router";
 
 export default function Home() {
 	return (
 		<main class="flex flex-col flex-grow justify-center items-center p-4 min-h-full">
 			<Title>vividsystem</Title>
+			<Meta
+				name="description"
+				content="vividsystem - software developer and maker from Germany"
+			/>
 			<div>
 				<h1 class="text-4xl lg:text-9xl text-primary font-normal py-4">Hi, I am <br /><span class="intro supports-hover:not-hover:bg-primary supports-hover:hover:text-vivid-pink">vividsystem</span></h1>
 				<p class="text-2xl lg:text-4xl text-secondary py-4">I <A class="link-vivid-pink" href="/projects">build things</A> in my free-time.</p>
