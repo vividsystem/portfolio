78fa1cfc314a991e147a88ca070d0226fe3874f8
vividsystem <notrealbutnotobot@gmail.com>
2026-05-16 18:25:12 +0200

    feat: SEO and performance optimization Added robots.txt for SEO and changed google fonts import to <link> from the @import to not block rendering

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -9,6 +9,10 @@ export default createHandler(() => (
 					<meta charset="utf-8" />
 					<meta name="viewport" content="width=device-width, initial-scale=1" />
 					<link rel="icon" href="/favicon.svg" />
+
+					<link rel="preconnect" href="https://fonts.googleapis.com" />
+					<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="" />
+					<link href="https://fonts.googleapis.com/css2?family=Bitcount+Grid+Single:wght@100..900&family=Momo+Trust+Display&display=swap" rel="stylesheet" />
 					{assets}
 				</head>
 				<body class="flex flex-col min-h-screen">
